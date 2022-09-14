42173a58bca31354f6eeaf2d13d3ee034e4cc9a6
rkpust <rezaul.cse.pust17@gmail.com>
2022-09-14 12:35:27 +0600

    Mid-Point Circle Drawing Algorithm

---
+++ MidPointCircle.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+using namespace std; 
+
+int main()
+{
+    int xc,yc, redius, x, y;
+    cout<<"Enter center of Circle"<<endl;
+    cin>>xc>>yc;
+    cout<<"Enter Redius of Circle"<<endl;
+    cin>>redius;
+
+    x=0;
+    y = redius;
+    int p = 1-redius;
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char*)" ");
+    
+    do{
+        putpixel(xc + x, yc + y,RED); 
+        putpixel (xc - x, yc + y,BLUE); 
+        putpixel (xc + x, yc - y,GREEN); 
+        putpixel (xc - x, yc - y,WHITE); 
+        putpixel (xc + y, yc + x,YELLOW); 
+        putpixel (xc - y, yc + x,BLUE); 
+        putpixel (xc + y, yc - x,RED); 
+        putpixel (xc - y, yc - x,GREEN);
+      
+      if(p<0)
+        {
+            p+= 2*x-1;
+        }
+      else
+        {
+           y--;
+           p+= 2*(x-y)+1;
+        }
+        x++;
+    }while(x<y);
+    getch();
+    closegraph();
+    return 0;
+}
